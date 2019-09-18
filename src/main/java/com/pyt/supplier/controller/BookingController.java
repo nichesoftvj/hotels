aa95568864554d2bc02930a8989a5acfbb2bccaa
nichesoft25 <nichesoft25@192.168.0.105>
2019-09-18 21:47:25 +0530

    add booking controller

---
+++ src/main/java/com/pyt/supplier/controller/BookingController.java
@@ -0,0 +1,46 @@
+package com.pyt.supplier.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pyt.supplier.model.response.Response;
+import com.pyt.supplier.service.BookingSerivice;
+
+import expedia.model.CreateItineraryRequest;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@EnableAutoConfiguration
+@Slf4j
+@RequestMapping("hotels")
+public class BookingController {
+
+	@Autowired
+	private BookingSerivice bookingService;
+
+	@PostMapping(value = "/{hotelId}/book", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation("Book a room for given hotel id")
+	@CrossOrigin(origins = "*")
+	@ResponseBody
+	public Response booking(@PathVariable String hotelId, @RequestBody CreateItineraryRequest createRequest) {
+
+		Response response = new Response();
+		try {
+			response = bookingService.booking(hotelId, createRequest);
+			System.out.println(createRequest.getAffiliateReferenceId());
+		} catch (Exception e) {
+			log.error("Exception: " + e.getMessage());
+		}
+		return response;
+	}
+}
